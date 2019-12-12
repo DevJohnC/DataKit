1a81e4283ea88f68bc979efb630f86f51d307703
John <johnc@frag-labs.com>
2019-12-12 12:38:28 -0600

    Add IObjectMapper for mapping data between reference types.
    Implemented out of the box by DefaultObjectMapper which can accept an IObjectMapper to control construction of new
    object instances and defines IBindingFactory to control how types are bound.
    
    By default uses DefaultObjectFactory.Instance for construction of new object instances and
    uses AutoBind() to create default type bindings.

---
+++ DataKit.Mapping.UnitTests/DefaultObjectMapperTests.cs
@@ -0,0 +1,109 @@
+﻿using DataKit.Mapping.Binding;
+using DataKit.Modelling.TypeModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataKit.Mapping.UnitTests
+{
+	[TestClass]
+	public class DefaultObjectMapperTests
+	{
+		[TestMethod]
+		public void Can_Inject_Single_Object()
+		{
+			var mapper = new DefaultObjectMapper();
+			var src = new Source { Value = 2 };
+			var trgt = new Target();
+			mapper.Inject(src, trgt);
+			Assert.AreEqual(src.Value, trgt.Value);
+		}
+
+		[TestMethod]
+		public void Can_Inject_Enumerations()
+		{
+			var mapper = new DefaultObjectMapper();
+			var src = new[] { new Source { Value = 2 } };
+			var trgt = new[] { new Target() };
+			mapper.InjectAll(src, trgt);
+			Assert.AreEqual(src[0].Value, trgt[0].Value);
+		}
+
+		[TestMethod]
+		public void Can_Map_Single_Object()
+		{
+			var mapper = new DefaultObjectMapper();
+			var src = new Source { Value = 2 };
+			var trgt = mapper.Map<Source, Target>(src);
+			Assert.AreEqual(src.Value, trgt.Value);
+		}
+
+		[TestMethod]
+		public void Can_Map_Enumerations()
+		{
+			var mapper = new DefaultObjectMapper();
+			var src = new[] { new Source { Value = 2 } };
+			var trgt = mapper.MapAll<Source, Target>(src)
+				.ToArray();
+			Assert.AreEqual(src.Length, trgt.Length);
+			Assert.AreEqual(src[0].Value, trgt[0].Value);
+		}
+
+		[TestMethod]
+		public void Can_Use_Custom_ObjectFactory()
+		{
+			var mapper = new DefaultObjectMapper(
+				objectFactory: new TargetSubClassFactory()
+				);
+			var src = new Source { Value = 2 };
+			var trgt = mapper.Map<Source, Target>(src);
+			Assert.IsInstanceOfType(trgt, typeof(TargetSubClass));
+		}
+
+		[TestMethod]
+		public void Can_Use_Custom_Bindings()
+		{
+			var mapper = new DefaultObjectMapper(
+				bindingFactory: new DoublesValueBindingFactory()
+				);
+			var src = new Source { Value = 2 };
+			var trgt = mapper.Map<Source, Target>(src);
+			Assert.AreEqual(src.Value * 2, trgt.Value);
+		}
+
+		private class Source
+		{
+			public int Value { get; set; }
+		}
+
+		private class Target
+		{
+			public int Value { get; set; }
+		}
+
+		private class TargetSubClass : Target { }
+
+		private class TargetSubClassFactory : IObjectFactory
+		{
+			public object CreateInstance(Type type)
+			{
+				return new TargetSubClass();
+			}
+		}
+
+		private class DoublesValueBindingFactory : DefaultObjectMapper.IBindingFactory
+		{
+			public DataModelBinding<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> GetBindingForMapping<TFrom, TTo>()
+				where TFrom : class
+				where TTo : class
+			{
+				return new TypeBindingBuilder<Source, Target>()
+					.Bind(t => t.Value, s => s.Value, value => value * 2)
+					.BuildBinding()
+					as DataModelBinding<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField>;
+			}
+		}
+	}
+}
---
+++ DataKit.Mapping/DefaultObjectMapper.cs
@@ -0,0 +1,109 @@
+﻿using DataKit.Mapping.Binding;
+using DataKit.Modelling;
+using DataKit.Modelling.TypeModels;
+using System;
+using System.Collections.Generic;
+
+namespace DataKit.Mapping
+{
+	public class DefaultObjectMapper : IObjectMapper, DefaultObjectMapper.IBindingFactory
+	{
+		private readonly IObjectFactory _objectFactory;
+		private readonly CachedCollection<(Type, Type), Mapping> _mappingCache
+			= new CachedCollection<(Type, Type), Mapping>();
+		private readonly IBindingFactory _bindingFactory;
+
+		public DefaultObjectMapper(IObjectFactory objectFactory = null,
+			IBindingFactory bindingFactory = null)
+		{
+			_objectFactory = objectFactory ?? DefaultObjectFactory.Instance;
+			_bindingFactory = bindingFactory ?? this;
+		}
+
+		private Mapping<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> GetMapping<TFrom, TTo>()
+			where TFrom : class
+			where TTo : class
+		{
+			var key = (typeof(TFrom), typeof(TTo));
+			if (_mappingCache.TryGetValue(key, out var mapping))
+				return mapping as Mapping<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField>;
+
+			return _mappingCache.CreateIfNeeded(key, MappingFactory<TFrom, TTo>)
+				as Mapping<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField>;
+		}
+
+		private Mapping<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> MappingFactory<TFrom, TTo>()
+			where TFrom : class
+			where TTo : class
+		{
+			return _bindingFactory.GetBindingForMapping<TFrom, TTo>()
+				.BuildMapping();
+		}
+
+		private void InjectSingle<TFrom, TTo>(TFrom from, TTo to,
+			Mapping<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> mapping)
+			where TFrom : class
+			where TTo : class
+		{
+			var reader = new ObjectDataModelReader<TFrom>(from);
+			var writer = new ObjectDataModelWriter<TTo>(to, _objectFactory);
+			mapping.Run(reader, writer);
+		}
+
+		public void Inject<TFrom, TTo>(TFrom from, TTo to)
+			where TFrom : class
+			where TTo : class
+		{
+			InjectSingle(from, to, GetMapping<TFrom, TTo>());
+		}
+
+		public void InjectAll<TFrom, TTo>(IEnumerable<TFrom> from, IEnumerable<TTo> to)
+			where TFrom : class
+			where TTo : class
+		{
+			var mapping = GetMapping<TFrom, TTo>();
+			using (var fromEnumerator = from.GetEnumerator())
+			using (var toEnumerator = to.GetEnumerator())
+			{
+				while (fromEnumerator.MoveNext() && toEnumerator.MoveNext())
+				{
+					InjectSingle(fromEnumerator.Current, toEnumerator.Current, mapping);
+				}
+			}
+		}
+
+		public TTo Map<TFrom, TTo>(TFrom from)
+			where TFrom : class
+			where TTo : class
+		{
+			var to = _objectFactory.CreateInstance<TTo>();
+			InjectSingle(from, to, GetMapping<TFrom, TTo>());
+			return to;
+		}
+
+		public IEnumerable<TTo> MapAll<TFrom, TTo>(IEnumerable<TFrom> from)
+			where TFrom : class
+			where TTo : class
+		{
+			var mapping = GetMapping<TFrom, TTo>();
+			foreach (var obj in from)
+			{
+				var to = _objectFactory.CreateInstance<TTo>();
+				InjectSingle(obj, to, mapping);
+				yield return to;
+			}
+		}
+
+		DataModelBinding<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> IBindingFactory.GetBindingForMapping<TFrom, TTo>()
+			=> new TypeBindingBuilder<TFrom, TTo>()
+				.AutoBind()
+				.BuildBinding();
+
+		public interface IBindingFactory
+		{
+			DataModelBinding<TypeModel<TFrom>, PropertyField, TypeModel<TTo>, PropertyField> GetBindingForMapping<TFrom, TTo>()
+				where TFrom : class
+				where TTo : class;
+		}
+	}
+}
