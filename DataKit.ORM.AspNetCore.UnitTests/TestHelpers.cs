103388c5b240bb2d3203a1f35b43f00ae64c34bd
John Carruthers <johnc@frag-labs.com>
2019-12-13 16:44:17 -0600

    Test ORM and mapping integration on AspNetCore
    These tests ensure that the ORM and mapping libraries correctly integrate via the AspNetCore services.

---
+++ DataKit.ORM.AspNetCore.UnitTests/TestHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Silk.Data.SQL.Providers;
+using Silk.Data.SQL.SQLite3;
+using System;
+
+namespace DataKit.ORM.AspNetCore.UnitTests
+{
+	public static class TestHelpers
+	{
+		public static IServiceProvider CreateContainer(
+			Action<IServiceCollection> addAdditionalServices = null
+			)
+		{
+			var services = new ServiceCollection();
+			addAdditionalServices?.Invoke(services);
+			return services.BuildServiceProvider();
+		}
+
+		public static IDataProvider CreateTestProvider()
+		{
+			return new SQLite3DataProvider("Data Source=:memory:;Mode=Memory");
+		}
+	}
+}
