9273dde4aec8ae0a48259fe7aaf98c5f1424c1d7
John Carruthers <johnc@frag-labs.com>
2019-12-22 23:01:20 -0600

    Add MySQL/MariaDB data provider.

---
+++ DataKit.SQL.MySQL/MySQLDataProvider.cs
@@ -0,0 +1,68 @@
+﻿using DataKit.SQL.Providers;
+using MySql.Data.MySqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataKit.SQL.MySQL
+{
+	public class MySQLDataProvider : DataProviderBase
+	{
+		public const string PROVIDER_NAME = "MySQL/MariaDB";
+
+		public static MySQLDataProvider Create(string host, string user, string pass, string database)
+		{
+			return new MySQLDataProvider(new MySqlConnectionStringBuilder
+			{
+				Server = host,
+				UserID = user,
+				Password = pass,
+				Database = database
+			});
+		}
+
+		public static MySQLDataProvider CreateWithoutSSL(string host, string user, string pass, string database)
+		{
+			return new MySQLDataProvider(new MySqlConnectionStringBuilder
+			{
+				Server = host,
+				UserID = user,
+				Password = pass,
+				Database = database,
+				SslMode = MySqlSslMode.None
+			});
+		}
+
+		public override string ProviderName => PROVIDER_NAME;
+
+		private readonly string _connectionString;
+
+		public MySQLDataProvider(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public MySQLDataProvider(MySqlConnectionStringBuilder connectionStringBuilder) :
+			this(connectionStringBuilder.ConnectionString)
+		{
+		}
+
+		protected override QueryWriter CreateQueryWriter()
+		{
+			return new MySQLQueryWriter();
+		}
+
+		protected override IConnectionLease OpenConnection()
+		{
+			var connection = new MySqlConnection(_connectionString);
+			connection.Open();
+			return new ClosingConnectionLease(connection);
+		}
+
+		protected override async Task<IConnectionLease> OpenConnectionAsync(CancellationToken cancellationToken)
+		{
+			var connection = new MySqlConnection(_connectionString);
+			await connection.OpenAsync(cancellationToken);
+			return new ClosingConnectionLease(connection);
+		}
+	}
+}
---
+++ DataKit.SQL.MySQL/QueryExpressions/IMySQLExtension.cs
@@ -0,0 +1,9 @@
+﻿using DataKit.SQL.QueryExpressions;
+
+namespace DataKit.SQL.MySQL.QueryExpressions
+{
+	public interface IMySQLExtension
+	{
+		void Write(MySQLQueryWriter queryWriter, QueryExpressionVisitor queryExpressionVisitor);
+	}
+}
